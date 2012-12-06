95d3dd0186e6f79a49ce0a87afd4d43a34c28ed3
José Manuel Chinea <jmchinea@gmail.com>
2012-12-06 16:13:49 -0430

    Seguros La Fé, primera versión

---
+++ README.txt
@@ -0,0 +1,1 @@
+Seguros La Fe
---
+++ js/messages.js
@@ -0,0 +1,11 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ *
+ */
+jQuery.extend(jQuery.validator.messages, {
+  required: localStorage.msgValidTag1,
+  remote: localStorage.msgValidTag2,
+  email: localStorage.msgValidTag3,
+  url: localStorage.msgValidTag4,
+  date: localStorage.msgValidTag5
+});
